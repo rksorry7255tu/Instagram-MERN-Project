ce015c78afa603328c9a67d0a3901faa810e354c
rksorry7255tu <rk7255053708@gmail.com>
2025-10-26 10:11:41 +0530

    commited

--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -1,7 +1,7 @@
 import { Post } from "../models/post.model.js";
 import uploadOnCloudinary from "../middlewares/cloudinary.js";
 import { User } from "../models/user.model.js";
-import { log } from "console";
+import { Comment } from "../models/comment.model.js";
 
 const addPost = async (req, res) => {
   try {
@@ -101,7 +101,22 @@ const getAllPost = async (req, res) => {
 const getUserPost = async (req, res) => {
   try {
     const userId = req.id;
-    const userPosts = await Post.find({ author: userId });
+    const userPosts = await Post.find({ author: userId })
+      .sort({ createdAt: -1 })
+      .populate({
+        path: "author",
+        select: "username profilePicture",
+      });
+    // .populate({
+    //   path: "comments",
+    //   sort: {
+    //     createAt: -1,
+    //   },
+    //   populate: {
+    //     path: "author",
+    //     select: "username profilePicture",
+    //   },
+    // });
     if (!userPosts) {
       return res.status(401).json({
         success: false,
@@ -111,10 +126,250 @@ const getUserPost = async (req, res) => {
     return res.status(200).json({
       success: true,
       message: "Posts found successfully",
+      userPosts,
     });
   } catch (error) {
     console.log(error);
   }
 };
 
-export { addPost, getAllPost, getUserPost };
+const likePost = async (req, res) => {
+  try {
+    const likeKarneWala = req.id;
+    const postId = req?.params.id;
+
+    if (!postId) {
+      return res.status(401).json({
+        success: true,
+        message: "PostId not found",
+      });
+    }
+
+    const post = await Post.findByIdAndUpdate(
+      { _id: postId },
+      { $push: { likes: likeKarneWala } }
+    );
+    if (!post) {
+      return res.status(401).json({
+        success: false,
+        message: "Post not found",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "post liked successfully",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const disLikePost = async (req, res) => {
+  try {
+    const likeKarneWala = req.id;
+    const postId = req.params.id;
+
+    if (!postId) {
+      return res.status(401).json({
+        success: false,
+        message: "PostId not found",
+      });
+    }
+    const post = await Post.findByIdAndUpdate(
+      { _id: postId },
+      { $pull: { likes: likeKarneWala } }
+    );
+    if (!post) {
+      return res.status(401).json({
+        success: false,
+        message: "Post not found",
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      message: "Post disLiked successfully",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const addComment = async (req, res) => {
+  try {
+    const commentkarneWalaKaId = req.id;
+    const jisPostPeCommentHoga = req.params.id;
+    const { text } = req.body;
+    if (!text) {
+      return res.status(401).json({
+        success: true,
+        message: "comment is required.",
+      });
+    }
+    if (!commentkarneWalaKaId || !jisPostPeCommentHoga) {
+      return res.status(401).json({
+        success: false,
+        message:
+          "commentkarneWalaKaId or jisPostPeCommentHoga id jo hai wo missing hai ",
+      });
+    }
+    let post = await Post.findById(jisPostPeCommentHoga);
+    if (!post) {
+      return res.status(401).json({
+        success: false,
+        message: "Post not found",
+      });
+    }
+
+    const comment = await Comment.create({
+      text: text,
+      author: commentkarneWalaKaId,
+      post: post?.id,
+    });
+    if (!comment) {
+      return res.status(401).json({
+        success: false,
+        message: "comment not created",
+      });
+    }
+
+    post = await Post.findByIdAndUpdate(
+      { _id: post?._id },
+      { $push: { comments: comment?._id } }
+    );
+    if (!post) {
+      return res.status(401).json({
+        success: false,
+        message: "Post not updated",
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      message: "comment added successfully",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getCommentsOfPosts = async (req, res) => {
+  try {
+    const postId = req?.params?.id;
+
+    // console.log(postId);
+
+    const comments = await Comment.find({ post: postId }).populate({
+      path: "author",
+      select: "username profilePicture",
+    });
+    // console.log(comments);
+
+    if (!comments) {
+      return res.status(401).json({
+        success: false,
+        message: "Post not found",
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      message: "comments found successfully",
+      comments,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const deletePost = async (req, res) => {
+  try {
+    const postId = req?.params?.id;
+    const userId = req.id;
+
+    const post = await Post.findById(postId);
+    if (!post) {
+      return res.status(401).json({
+        success: true,
+        message: "post not found",
+      });
+    }
+    if (post?.author?.toString() !== userId) {
+      return res.status(401).json({
+        success: false,
+        message: "Unauthorised user.",
+      });
+    }
+    await Post.findByIdAndDelete({ _id: postId });
+
+    const comments = await Comment.deleteMany({ post: postId });
+
+    const user = await User.findByIdAndUpdate(
+      { _id: userId },
+      { $pull: { posts: post?._id } }
+    );
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "user not found",
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      message: "Post deleted successfully",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const bookMarkPost = async (req, res) => {
+  try {
+    const postId = req.params.id;
+    const userId = req.id;
+
+    let user = await User.findById(userId);
+    if (!user) {
+      return res.status(401).json({
+        success: true,
+        message: "user not found",
+      });
+    }
+
+    const isBookMarked = user?.bookmarks?.includes(postId);
+
+    if (isBookMarked) {
+      //remove karna hai
+      user = await User.findByIdAndUpdate(
+        { _id: userId },
+        { $pull: { bookmarks: postId } }
+      );
+      return res.status(200).json({
+        success: true,
+        message: "Post removed from bookmarked successfully",
+      });
+    } else {
+      //add karna hai
+      user = await User.findByIdAndUpdate(
+        { _id: userId },
+        { $push: { bookmarks: postId } }
+      );
+      return res.status(200).json({
+        success: true,
+        message: "Post bookmarked successfully",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export {
+  addPost,
+  getAllPost,
+  getUserPost,
+  likePost,
+  disLikePost,
+  addComment,
+  getCommentsOfPosts,
+  deletePost,
+  bookMarkPost,
+};
--- backend/src/routes/post.routes.js
+++ backend/src/routes/post.routes.js
@@ -1,5 +1,15 @@
 import express from "express";
-import { addPost, getAllPost } from "../controllers/post.controller.js";
+import {
+  addPost,
+  getAllPost,
+  getUserPost,
+  likePost,
+  disLikePost,
+  addComment,
+  getCommentsOfPosts,
+  deletePost,
+  bookMarkPost,
+} from "../controllers/post.controller.js";
 import isAuthentication from "../middlewares/isAuthentication.js";
 import { upload } from "../middlewares/multer.middleware.js";
 
@@ -10,5 +20,21 @@ router
   .post(upload.single("image"), isAuthentication, addPost);
 
 router.route("/getallposts").get(isAuthentication, getAllPost);
+
+router.route("/getuserposts").get(isAuthentication, getUserPost);
+
+router.route("/likepost/:id").post(isAuthentication, likePost);
+
+router.route("/dislikepost/:id").post(isAuthentication, disLikePost);
+
+router.route("/addcomment/:id").post(isAuthentication, addComment);
+
+router
+  .route("/getcommentsofpost/:id")
+  .get(isAuthentication, getCommentsOfPosts);
+
+router.route("/deletepost/:id").delete(isAuthentication, deletePost);
+
+router.route("/bookmarkpost/:id").post(isAuthentication, bookMarkPost);
 
 export default router;
